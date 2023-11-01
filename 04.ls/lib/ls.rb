6de6c833454c7d60f52b8664779c894c9de29262
satoakito <sato_akito@vitallead.co.jp>
2023-11-01 10:26:38 +0900

    一部処理をメソッド化・変数名等変更

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -11,30 +11,35 @@ def prepare_files
   col_num = 1
   col_num = (files.length / ROW_NUM.to_f).ceil if files.length > ROW_NUM
 
-  split_list = files.sort_by { |s| [s.downcase, s] }.each_slice(col_num).to_a
+  divided_files = files.sort_by { |s| [s.downcase, s] }.each_slice(col_num).to_a
 
-  { col_num:, split_list:, item_width: calc_item_width(files) }
+  { col_num:, divided_files:, col_width: calc_col_width(files) }
 end
 
-def calc_item_width(files)
+def calc_col_width(files)
   files.collect do |file|
-    file_name_length = 0
+    col_width = 0
     file.each_char do |char|
-      file_name_length += 1
-      file_name_length += 1 unless char.bytesize == 1
+      col_width += 1
+      col_width += 1 unless char.bytesize == 1
     end
-    file_name_length += 2
+    col_width += 2
   end.max
 end
 
-def display_files(col_num, split_list, item_width)
-  col_num.times do |col|
-    split_list.length.times do |row|
-      break if !split_list[row][col]
+def calc_col_width_mb(col_width, file)
+  mb_count = 0
+  file.each_char { |char| mb_count += 1 unless char.bytesize == 1 }
+  col_width - mb_count
+end
 
-      mb_count = 0
-      split_list[row][col].each_char { |file| mb_count += 1 unless file.bytesize == 1 }
-      print split_list[row][col].ljust(item_width - mb_count)
+def display_files(col_num, divided_files, col_width)
+  col_num.times do |col|
+    divided_files.length.times do |row|
+      break if !divided_files[row][col]
+
+      col_width_mb = calc_col_width_mb(col_width, divided_files[row][col])
+      print divided_files[row][col].ljust(col_width_mb)
     end
     puts
   end
@@ -42,4 +47,4 @@ end
 
 ROW_NUM = 3
 list = prepare_files
-display_files(list[:col_num], list[:split_list], list[:item_width])
+display_files(list[:col_num], list[:divided_files], list[:col_width])
