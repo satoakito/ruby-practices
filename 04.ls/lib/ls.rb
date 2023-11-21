374f1795e750e0b4a7e48170a304a109a78bb870
satoakito <sato_akito@vitallead.co.jp>
2023-11-21 16:25:51 +0900

    指摘内容等の修正

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -3,21 +3,32 @@
 require 'optparse'
 require 'etc'
 
-def prepare_files(option_a, option_r)
-  files = load_files(option_a)
-  col_num = 1
-  col_num = (files.length / ROW_NUM.to_f).ceil if files.length > ROW_NUM
-
-  divided_files = sort_files(files, option_r).each_slice(col_num).to_a
-
-  { col_num:, divided_files:, col_width: calc_col_width(files) }
+def load_files(option_a)
+  if ARGV == [] || FileTest.directory?(ARGV[0])
+    Dir.glob('*', option_a ? File::FNM_DOTMATCH : 0, base: ARGV[0] || nil)
+  elsif FileTest.file?(ARGV[0])
+    ARGV
+  end
 end
 
-def prepare_files_l_option(option_a, option_r)
-  files = load_files(option_a)
+def sort_files(files, option_r)
+  sorted_files = files.sort_by { |s| [s.match(/[^_.]+/).to_s.downcase, s] }
+  sorted_files = sorted_files.reverse if option_r
+  sorted_files
+end
 
+def prepare_files(sorted_files)
+  col_num = 1
+  col_num = (sorted_files.length / ROW_NUM.to_f).ceil if sorted_files.length > ROW_NUM
+
+  prepared_files = sorted_files.each_slice(col_num).to_a
+
+  { col_num:, prepared_files:, col_width: calc_col_width(sorted_files) }
+end
+
+def prepare_files_l_option(sorted_files)
   prepared_files = []
-  sort_files(files, option_r).each do |file|
+  sorted_files.each do |file|
     file_stat = File.lstat(ARGV[0] ? "#{ARGV[0]}/#{file}" : file)
     file_elements = {
       blocks: file_stat.blocks / 2, # OS標準とstatの差を吸収するため2で割っている
@@ -32,20 +43,6 @@ def prepare_files_l_option(option_a, option_r)
     prepared_files << file_elements
   end
   prepared_files
-end
-
-def load_files(option_a)
-  if ARGV == [] || FileTest.directory?(ARGV[0])
-    Dir.glob('*', option_a ? File::FNM_DOTMATCH : 0, base: ARGV[0] || nil)
-  elsif FileTest.file?(ARGV[0])
-    ARGV
-  end
-end
-
-def sort_files(files, option_r)
-  sorted_files = files.sort_by { |s| [s.match(/[^_.]+/).to_s.downcase, s] }
-  sorted_files = sorted_files.reverse if option_r
-  sorted_files
 end
 
 def calc_col_width(files)
@@ -70,48 +67,22 @@ def calc_col_width_l_option(files, item)
 end
 
 def convert_mode(file_stat)
-  converted_mode = convert_file_type(file_stat)
-  convert_permission(file_stat, converted_mode).join
-end
-
-def convert_file_type(file_stat)
-  file_type_symbol = {
-    'fifo' => ['p'],
-    'characterSpecial' => ['c'],
-    'directory' => ['d'],
-    'blockSpecial' => ['b'],
-    'file' => ['-'],
-    'link' => ['l'],
-    'socket' => ['s']
-  }
-  file_type_symbol[file_stat.ftype]
-end
-
-def convert_permission(file_stat, converted_mode)
-  permission_symbol = {
-    '7' => 'rwx',
-    '6' => 'rw-',
-    '5' => 'r-w',
-    '4' => 'r--',
-    '3' => '-wx',
-    '2' => '-w-',
-    '1' => '--x',
-    '0' => '---'
-  }
+  converted_mode = []
+  converted_mode << FILE_TYPE_SYMBOL[file_stat.ftype]
   mode_octal = format('%06d', file_stat.mode.to_s(8))
   (3..5).each do |i|
-    converted_mode << permission_symbol[mode_octal[i]]
+    converted_mode << PERMISSION_SYMBOL[mode_octal[i]]
   end
-  converted_mode
+  converted_mode.join
 end
 
-def display_files(col_num, divided_files, col_width)
+def display_files(col_num, files, col_width)
   col_num.times do |col|
-    divided_files.length.times do |row|
-      break if !divided_files[row][col]
+    files.length.times do |row|
+      break if !files[row][col]
 
-      col_width_mb = calc_col_width_mb(col_width, divided_files[row][col])
-      print divided_files[row][col].ljust(col_width_mb)
+      col_width_mb = calc_col_width_mb(col_width, files[row][col])
+      print files[row][col].ljust(col_width_mb)
     end
     puts
   end
@@ -139,10 +110,31 @@ opt.on('-r') { |v| option[:r] = v }
 opt.on('-l') { |v| option[:l] = v }
 opt.parse!(ARGV)
 
+sorted_files = sort_files(load_files(option[:a]), option[:r])
+
 if option[:l]
-  display_files_l_option(prepare_files_l_option(option[:a], option[:r]))
+  FILE_TYPE_SYMBOL = {
+    'fifo' => ['p'],
+    'characterSpecial' => ['c'],
+    'directory' => ['d'],
+    'blockSpecial' => ['b'],
+    'file' => ['-'],
+    'link' => ['l'],
+    'socket' => ['s']
+  }.freeze
+  PERMISSION_SYMBOL = {
+    '7' => 'rwx',
+    '6' => 'rw-',
+    '5' => 'r-w',
+    '4' => 'r--',
+    '3' => '-wx',
+    '2' => '-w-',
+    '1' => '--x',
+    '0' => '---'
+  }.freeze
+  display_files_l_option(prepare_files_l_option(sorted_files))
 else
   ROW_NUM = 3
-  list = prepare_files(option[:a], option[:r])
-  display_files(list[:col_num], list[:divided_files], list[:col_width])
+  list = prepare_files(sorted_files)
+  display_files(list[:col_num], list[:prepared_files], list[:col_width])
 end
